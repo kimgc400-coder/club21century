f959fa49148d5b83c7b5e7f0f0d991fe645cc52b
kimgc400-coder <228484950+kimgc400-coder@users.noreply.github.com>
2026-09-05 00:18:56 +0900

    Initial commit
    최초 커밋

---
+++ src/main/java/com/toyproject/club21century/Club21centuryApplication.java
@@ -0,0 +1,13 @@
+package com.toyproject.club21century;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Club21centuryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Club21centuryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toyproject/club21century/Club21centuryApplicationTests.java
@@ -0,0 +1,15 @@
+package com.toyproject.club21century;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class Club21centuryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
